476b0468298f368f882dc655a043b05f22ac07c4
Bad-Raider <stanislav.gusiev@gmail.com>
2023-09-15 19:12:01 +0300

    fix json file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/auto-rent">
+    <BrowserRouter basename="/auto-rent-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
